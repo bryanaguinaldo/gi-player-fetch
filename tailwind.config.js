d22917513932a717b979071429d3be23514fc5b7
Bryan Aguinaldo <bryanaguinaldo.e@gmail.com>
2022-08-05 15:00:01 +0800

    added profile images, created user template

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        colors: {
+            "gi-tan": "#D3BC8E",
+            "gi-alabaster": "#F0EBE1",
+            "gi-antique-brass": "#D19D78",
+            white: "#ffffff",
+            black: "#000000",
+        },
+        backgroundSize: {
+            "50%": "50%",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
