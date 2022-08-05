d83affb15616858df6042e23ec2b4414b1a683e9
Bryan Aguinaldo <bryanaguinaldo.e@gmail.com>
2022-08-05 17:03:51 +0800

    added theme colors, revised template, added player card component"

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import "./App.css";
+import BasePlayerCard from "./components/BasePlayerCard";
 
 function App() {
     return (
         <div className="App">
             <div className="h-screen flex justify-center items-center">
-                <div className="h-[155px] w-[330px] md:h-[208px] md:w-[443px] lg:h-[316px] lg:w-[672px] bg-white rounded-md shadow-md bg-[url('../public/namecards/Namecard_Background_Genshin_Impact_A_New_World.png')] bg-cover">
-                    <div className="p-4">
-                        <div className="grid grid-cols-12">
-                            <div className="col-span-3">
-                                <div className="w-[75px] h-[75px] md:h-[100px] md:w-[100px] lg:h-[155px] lg:w-[155px] rounded-full bg-gi-alabaster shadow-lg">
-                                    <div className="h-full flex justify-center items-center">
-                                        <div className="w-[67px] h-[67px] md:w-[90px] md:h-[90px] lg:w-[145px] lg:h-[145px] bg-gi-antique-brass rounded-full shadow-inner shadow-lg bg-[url('../public/characters/Character_Lumine_Thumb.png')] bg-center bg-[length:125%]" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-span-9">
-                                <div className="h-[75px] md:h-[100px] lg:h-[155px] flex flex-row justify-start mx-4 mt-1 md:mt-2 lg:mx-4 mt-4">
-                                    <div className="w-full">
-                                        <div className="w-full flex justify-start items-center bg-black bg-opacity-[15%]">
-                                            <span className="text-lg md:text-2xl lg:text-4xl text-gi-alabaster mx-2">
-                                                Minatozaki
-                                            </span>
-                                        </div>
-                                        <span className="flex justify-start mt-2 ml-2 text-xl text-gi-alabaster">
-                                            <span className="text-sm md:text-lg text-gi-alabaster">
-                                                I love u Minatozaki Sana
-                                            </span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <BasePlayerCard />
             </div>
         </div>
     );
---
+++ src/components/BasePlayerCard.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+function BasePlayerCard(props) {
+    return (
+        <div className="w-[300px] h-[404px] sm:w-[460px] sm:h-[618px] lg:h-[724px] lg:w-[538px] bg-gi-alabaster rounded-md shadow-lg">
+            <div className="h-[112px] sm:h-[172px] sm:h-[201px] w-full bg-[url('../public/namecards/Namecard_Background_Genshin_Impact_A_New_World.png')] bg-contain rounded-md">
+                <div className="flex justify-center pt-3">
+                    <span className="text-xs sm:text-sm lg:text-md text-white shadow-sm">
+                        UID: {props.uid == null ? "000000000" : props.uid}
+                    </span>
+                </div>
+            </div>
+            <div className="-translate-y-[21%]">
+                <div className="flex justify-center">
+                    <div>
+                        <div className="w-[87px] h-[87px] sm:h-[135px] sm:w-[135px] lg:h-[156px] lg:w-[156px] rounded-full bg-gi-tan shadow-lg flex justify-center items-center">
+                            <div className="w-[83px] h-[83px] sm:h-[129px] sm:w-[129px] lg:h-[151px] lg:w-[151px] rounded-full bg-gi-alabaster shadow-md">
+                                <div className="h-full flex justify-center items-center">
+                                    <div className="w-[76px] h-[76px] sm:w-[118px] sm:h-[118px] lg:w-[138px] lg:h-[138px] bg-gi-antique-brass rounded-full shadow-inner shadow-lg bg-[url('../public/characters/Character_Lumine_Thumb.png')] bg-center bg-[length:125%]" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-4">
+                    <span className="text-lg sm:text-2xl lg:text-3xl text-gi-police-blue">
+                        {props.nickname == null ? "Undefined" : props.nickname}
+                    </span>
+                </div>
+                <div className="px-[10%] mt-2 lg:my-2 lg:mt-6">
+                    <div className="w-full bg-gi-olivine flex">
+                        <span className="justify-start ml-3 text-white text-sm sm:text-lg lg:text-xl">
+                            Adventure Rank
+                        </span>
+                        <span className="ml-auto mr-3 text-white text-sm sm:text-lg lg:text-xl">
+                            {props.adventureRank == null
+                                ? 0
+                                : props.adventureRank}
+                        </span>
+                    </div>
+                </div>
+                <div className="px-[10%] my-2">
+                    <div className="w-full bg-gi-dark-vanilla flex">
+                        <span className="justify-start ml-3 text-white text-sm sm:text-lg lg:text-xl">
+                            World Level
+                        </span>
+                        <span className="ml-auto mr-3 text-white text-sm sm:text-lg lg:text-xl">
+                            {props.worldLevel == null ? 0 : props.worldLevel}
+                        </span>
+                    </div>
+                </div>
+                <div className="px-[10%] mt-4">
+                    <div className="w-full h-[84px] sm:h-[127px] lg:h-[150px] border-gi-dark-vanilla border-2 flex">
+                        <span className="justify-start ml-3 text-gi-police-blue text-xs sm:text-lg lg:text-xl">
+                            {props.signature}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BasePlayerCard;
